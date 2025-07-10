7d3f08caacba283f3169849af0e747d65b63c61f
aryansinha9 <aryansinha8424@gmail.com>
2025-07-10 19:32:05 +1000

    Create OnboardingSession.js

---
+++ models/OnboardingSession.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const pageSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    access_token: { type: String, required: true }
+});
+
+const onboardingSessionSchema = new mongoose.Schema({
+    facebookUserId: { type: String, required: true, unique: true },
+    name: { type: String },
+    email: { type: String },
+    avatarUrl: { type: String },
+    pages: [pageSchema],
+    // The session will automatically expire after 15 minutes
+    createdAt: { type: Date, expires: '15m', default: Date.now }
+});
+
+module.exports = mongoose.model('OnboardingSession', onboardingSessionSchema);
