2ac853834888344220a450d658a747a693c6ab0e
aryansinha9 <aryansinha8424@gmail.com>
2025-07-15 18:42:05 +1000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -34,9 +34,9 @@ const userSchema = new mongoose.Schema({
         },
 
         plan: {
-        type: String,
-        enum: ['basic', 'pro', 'enterprise'],
-        default: 'basic'
+            type: String,
+            enum: ['basic', 'pro', 'enterprise'],
+            default: 'basic'
         },
 
         // The unique ID of their Facebook account (for reference)
@@ -47,9 +47,19 @@ const userSchema = new mongoose.Schema({
             instagram: { type: Boolean, default: true },
             facebook: { type: Boolean, default: true },
         },
+    },
 
-
+    // --- ADDED THIS SECTION ---
+    // This field will store a record of the user's agreement to the terms.
+    termsAgreement: {
+        // The version of the Terms and Conditions they agreed to (e.g., "1.0.0")
+        version: { type: String, default: '1.0.0' },
+        
+        // The exact date and time they provided their consent
+        agreedAt: { type: Date, required: true }
     }
+    // --- END OF ADDED SECTION ---
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
